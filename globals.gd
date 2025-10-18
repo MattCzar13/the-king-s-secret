d969589b051c8a028a64868032ab51c1bb1d6cf9
MattCzar13 <103082817+MattCzar13@users.noreply.github.com>
2025-10-18 17:17:38 -0400

    Initial setup

---
+++ globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# This is our global script. It is autoloaded, so you can reference it from
+# any script you make by using "Globals." at the start. For example, to access
+# a variable declared inside this script, do
+## Globals.variable_name
