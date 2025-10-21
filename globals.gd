1c9337fab0aeed9ab7ac35a4b3c2910994bbee5f
MattCzar13 <103082817+MattCzar13@users.noreply.github.com>
2025-10-21 16:48:04 -0400

    Global signal debugging scene
    Added a scene with a list of buttons for every global signal detected at startup. Also worked base project stuff: default root node + scene, global signals added, etc

--- globals.gd
+++ globals.gd
@@ -4,3 +4,22 @@ extends Node
 # any script you make by using "Globals." at the start. For example, to access
 # a variable declared inside this script, do
 ## Globals.variable_name
+
+signal example_signal
+
+signal message_delivered
+
+signal minigame_start_type_a
+signal minigame_start_type_b
+signal minigame_start_type_c
+signal minigame_success
+signal minigame_fail
+
+signal level_completed
+signal level_failed
+signal level_load(number : int)
+
+var level_information : Dictionary = {
+	"messages_delivered" : 0,
+	"message_delivery_goal" : 4
+}
